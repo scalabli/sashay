55dcfb9eb75762b15533f94fed50fd969044f4ac
Gerrishon Sirere <34297473+chouette254@users.noreply.github.com>
2020-09-30 14:27:11 +0300

    Update logo.py

--- modules/logo.py
+++ modules/logo.py
@@ -7,51 +7,7 @@ class logo:
   def tool_header(self):
     print('''\007
 
-\033[1;33m
-                                                                   
-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-XX   ::                                                                ::   XX
-XX   MMhh.        ..hhhhhh..                      ..hhhhhh..        .hhMM   XX
-XX   MMMMMh   ..hhMMMMMMMMMMhh.                .hhMMMMMMMMMMhh..   hMMMMM   XX
-XX   ---MMM .hMMMMdd:::dMMMMMMMhh..        ..hhMMMMMMMd:::ddMMMMh. MMM---   XX
-XX   MMMMMM MMmm''      'mmMMMMMMMMyy.  .yyMMMMMMMMmm'      ''mmMM MMMMMM   XX
-XX   ---mMM ''             'mmMMMMMMMM  MMMMMMMMmm'             '' MMm---   XX
-XX   yyyym'    .              'mMMMMm'  'mMMMMm'              .    'myyyy   XX
-XX   mm''    .y'     ..yyyyy..  ''''      ''''  ..yyyyy..     'y.    ''mm   XX
-XX           MN    .sMMMMMMMMMss.   .    .   .ssMMMMMMMMMs.    NM           XX
-XX           N`    MMMMMMMMMMMMMN   M    M   NMMMMMMMMMMMMM    `N           XX
-XX            +  .sMNNNNNMMMMMN+   `N    N`   +NMMMMMNNNNNMs.  +            XX
-XX              o+++     ++++Mo    M      M    oM++++     +++o              XX
-XX                                oo      oo                                XX
-XX           oM                 oo          oo                 Mo           XX
-XX         oMMo      ()        M              M       ()        oMMo        XX
-XX       +MMMM                 s              s                 MMMM+       XX
-XX      +MMMMM+            +++NNNN+        +NNNN+++            +MMMMM+      XX
-XX     +MMMMMMM+       ++NNMMMMMMMMN+    +NMMMMMMMMNN++       +MMMMMMM+     XX
-XX     MMMMMMMMMNN+++NNMMMMMMMMMMMMMMNNNNMMMMMMMMMMMMMMNN+++NNMMMMMMMMM     XX
-XX     yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy     XX
-XX   m  yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy  m   XX
-XX   MMm yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy mMM   XX
-XX   MMMm .yyMMMMMMMMMMMMMMMM     MMMMMMMMMM     MMMMMMMMMMMMMMMMyy. mMMM   XX
-XX   MMMMd   ''''hhhhh       odddo          obbbo        hhhh''''   dMMMM   XX
-XX   MMMMMd             'hMMMMMMMMMMddddddMMMMMMMMMMh'             dMMMMM   XX
-XX   MMMMMMd              'hMMMMMMMMMMMMMMMMMMMMMMh'              dMMMMMM   XX
-XX   MMMMMMM-               ''ddMMMMMMMMMMMMMMdd''               -MMMMMMM   XX
-XX   MMMMMMMM                   '::dddddddd::'                   MMMMMMMM   XX
-XX   MMMMMMMM-                                                  -MMMMMMMM   XX
-XX   MMMMMMMMM                                                  MMMMMMMMM   XX
-XX   MMMMMMMMMy                                                yMMMMMMMMM   XX
-XX   MMMMMMMMMMy.                                            .yMMMMMMMMMM   XX
-XX   MMMMMMMMMMMMy.                                        .yMMMMMMMMMMMM   XX
-XX   MMMMMMMMMMMMMMy.                                    .yMMMMMMMMMMMMMM   XX
-XX   MMMMMMMMMMMMMMMMs.                                .sMMMMMMMMMMMMMMMM   XX
-XX   MMMMMMMMMMMMMMMMMMss.           ....           .ssMMMMMMMMMMMMMMMMMM   XX
-XX   MMMMMMMMMMMMMMMMMMMMNo         oNNNNo         oNMMMMMMMMMMMMMMMMMMMM   XX
-XX                                                                          XX
-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-
+\033[1;33m                                                                                                            
                                    888                       
                                    888                      
               .oooo.o    .oooo.o   888888bo.    ooo    ooo
@@ -59,10 +15,9 @@ XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
              `"Y88b.    `"Y88b.    888    888    `88..8'
              o.  )88b   o.  )88b   888    888     `888'
              8""888P'   8""888P'  o888o  o888o    "888"      d8'
-                                              .o...P'
+                                              .o...P'        \033[1;91mBy Gerrishon
 
 
-\033[1;91mBy Gerrishon
 
 
 \033[1;36m ==================================================================\033[1;m
