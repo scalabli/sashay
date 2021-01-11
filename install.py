60fa917a1e40b7e4834757b3869ba8a2fff6e33b
Gerrishon Sirere <34297473+viewerdiscretion@users.noreply.github.com>
2021-01-11 15:38:22 +0300

    Update install.py

--- install.py
+++ install.py
@@ -1,11 +1,11 @@
-#This file is part of Sashay and may be subject to redistribution and commercial restrictions. Please visit our website
+#This file is part of sashay and may be subject to redistribution and commercial restrictions. Please visit our website
 #for more information on licensing and terms of use.
 
 import os
 import sys
 from time import sleep
-from modules.logo import *
-from modules.system import *
+from src.logo import *
+from src.system import *
 
 class tool:
   @classmethod
@@ -14,23 +14,23 @@ class tool:
       system=sys()
       os.system("clear")
       logo.ins_tnc()
-      inp=input("\033[1;33m Do you want to install Sashay? [Y/n]> \033[00m")
+      inp=input("\033[1;33m Do you want to install sashay? [Y/n]> \033[00m")
       if inp=="y" or inp=="Y":
         os.system("clear")
         logo.installing()
         if system.sudo is not None:
           #require root permission
-          if os.path.exists(system.conf_dir+"/Sashay"):
+          if os.path.exists(system.conf_dir+"/sashay"):
             pass
           else:
-            os.system(system.sudo+" mkdir "+system.conf_dir+"/Sashay")
-          os.system(system.sudo+" cp -r modules core Sashay.py "+system.conf_dir+"/Sashay")
-          os.system(system.sudo+" cp -r core/Sashay "+system.bin)
-          os.system(system.sudo+" cp -r core/sshy "+system.bin)
-          os.system(system.sudo+" chmod +x "+system.bin+"/Sashay")
+            os.system(system.sudo+" mkdir "+system.conf_dir+"/sashay")
+          os.system(system.sudo+" cp -r src assets sashay.py "+system.conf_dir+"/sashay")
+          os.system(system.sudo+" cp -r assets/sashay "+system.bin)
+          os.system(system.sudo+" cp -r assets/sshy "+system.bin)
+          os.system(system.sudo+" chmod +x "+system.bin+"/sashay")
           os.system(system.sudo+" chmod +x "+system.bin+"/sshy")
-          os.system("cd .. && "+system.sudo+" rm -rf Sashay")
-          if os.path.exists(system.bin+"/Sashay") and os.path.exists(system.conf_dir+"/Sashay"):
+          os.system("cd .. && "+system.sudo+" rm -rf sashay")
+          if os.path.exists(system.bin+"/sashay") and os.path.exists(system.conf_dir+"/sashay"):
             os.system("clear")
             logo.ins_sc()
             tmp=input("\033[1;36m ##> \033[00m")
@@ -41,17 +41,17 @@ class tool:
             tmp=input("\033[1;36m ##> \033[00m")
             break
         else:
-          if os.path.exists(system.conf_dir+"/Sashay"):
+          if os.path.exists(system.conf_dir+"/sashay"):
             pass
           else:
-            os.system("mkdir "+system.conf_dir+"/Sashay")
-          os.system("cp -r modules core Sashay.py "+system.conf_dir+"/Sashay")
-          os.system("cp -r core/Sashay "+system.bin)
-          os.system("cp -r core/sshy "+system.bin)
-          os.system("chmod +x "+system.bin+"/Sashay")
+            os.system("mkdir "+system.conf_dir+"/sashay")
+          os.system("cp -r src assets sashay.py "+system.conf_dir+"/sashay")
+          os.system("cp -r assets/sashay "+system.bin)
+          os.system("cp -r assets/sshy "+system.bin)
+          os.system("chmod +x "+system.bin+"/sashay")
           os.system("chmod +x "+system.bin+"/sshy")
-          os.system("cd .. && rm -rf Sashay")
-          if os.path.exists(system.bin+"/Sashay") and os.path.exists(system.conf_dir+"/Sashay"):
+          os.system("cd .. && rm -rf sashay")
+          if os.path.exists(system.bin+"/sashay") and os.path.exists(system.conf_dir+"/sashay"):
             os.system("clear")
             logo.ins_sc()
             tmp=input("\033[1;36m ##> \033[00m")
