6351058907655870659aa00ea9760555f444b4c6
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-07-19 19:25:56 +0300

    Update init.py

--- init.py
+++ init.py
@@ -1,23 +1,6 @@
 import os
 
-crl = "clear"
-perm = "chmod +x install"
-inst1 = "sh install" 
-inst2 = "./install"
+os.system("chmod +x install") 
+os.system("sh install")
 
 
-os.system("install quo")
-os.system("pip3 install quo")
-
-os.system(crl)
-os.system(perm)
-
-import quo
-from quo import echo, pause
-pause()
-
-os.system(inst1)
-os.system(inst2)
-  
-echo(f"You're all set", foreground="cyan")
-pass
