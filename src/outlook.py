2f51c918f0e6935fd08552b9dfc8eabb0896f75e
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-07-19 19:36:01 +0300

    Update outlook.py

--- src/outlook.py
+++ src/outlook.py
@@ -18,7 +18,7 @@ banner = ("""
 ┊ . ˚ ˚✩
 """)
 
-echo(f'{banner}', foreground="vred", bold=True)
+echo(f'{banner}', fg="vred", bold=True)
 
 cmd = "date"
 
@@ -26,10 +26,10 @@ cmd = "date"
 returned_output = subprocess.check_output(cmd)
 
 # using decode() function to convert byte string to string
-echo(f'Current date is: {returned_output.decode("utf-8")}', foreground="vgreen", bold=True)
+echo(f'Current date is: {returned_output.decode("utf-8")}', fg="vgreen", bold=True)
 
 async def main():
-    echo(f'Made with ♥', bold=True, foreground="vyellow")
+    echo(f'Made with ♥', bold=True, fg="vyellow")
     await asyncio.sleep(2) 
     echo(f"\N{ESC}[31mSecretum\u001b[0mInc.", bold=True)
 dedicate = main()
@@ -67,43 +67,43 @@ class logo:
   @classmethod
   def ins_tnc(self):
     self.tool_header() 
-    echo(f'THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERRCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL I BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THIS SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.', foreground='vblack', background='vwhite')
-    echo(f'Installing this tool means you agree with all terms', foreground='vred') 
+    echo(f'THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERRCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL I BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THIS SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.', fg='vblack', bg='vwhite')
+    echo(f'Installing this tool means you agree with all terms', fg='vred') 
     self.tool_footer()
 
   @classmethod
   def ins_sc(self):
     self.tool_header()
-    echo(f'[ ✓ ] sashay has been installed successfully', foreground='white')
-    echo(f'[ ✓ ] Type sashay or sshy from anywhere in your terminal', foreground='yellow') 
+    echo(f'[ ✓ ] sashay has been installed successfully', fg='white')
+    echo(f'[ ✓ ] Type sashay or sshy from anywhere in your terminal', fg='yellow') 
     self.tool_footer()
 
   @classmethod
   def update(self):
     self.tool_header()
-    echo(f'[ 1 ] Update sashay', foreground='vblue')
-    echo(f'[ 0 ] << Go back', foreground='vyellow')
+    echo(f'[ 1 ] Update sashay', fg='vblue')
+    echo(f'[ 0 ] << Go back', fg='vyellow')
     self.tool_footer()
 
   @classmethod
   def updated(self):
     self.tool_header()
-    echo(f'[ ✓ ] Congratulations! sashay has been updated successfully', foreground='vblack', background='vgreen') 
-    echo(f'[ ✓ ] Press enter to continue', foreground='vgreen')
+    echo(f'[ ✓ ] Congratulations! sashay has been updated successfully', fg='vblack', bg='vgreen') 
+    echo(f'[ ✓ ] Press enter to continue', fg='vgreen')
     self.tool_footer()
 
   @classmethod
   def nonet(self):
     self.tool_header()
-    echo(f'[ x ] There is no network connectivity', bold=True, foreground='cyan')
-    echo(f'[ x ] Please try again later', foreground='cyan')
+    echo(f'[ x ] There is no network connectivity', bold=True, fg='cyan')
+    echo(f'[ x ] Please try again later', fg='cyan')
     self.tool_footer()
 
   @classmethod
   def update_error(self):
     self.tool_header()
-    echo(f"[ x ] ssshy can't be updated at this time", foreground="cyan")
-    echo(f"[ x ] Please try again later", foreground="cyan") 
+    echo(f"[ x ] ssshy can't be updated at this time", fg="cyan")
+    echo(f"[ x ] Please try again later", fg="cyan") 
     self.tool_footer()
 
 
@@ -111,7 +111,7 @@ class logo:
   def about(self,total):
     self.tool_header()
     for i in range(3):
-        echo(f"{i}",  foreground="red", bold=True, background="white")
+        echo(f"{i}",  fg="red", bold=True, bg="white")
         sys.stdout.flush()
         time.sleep(1)
     __banner__ = """
@@ -130,21 +130,21 @@ class logo:
 
 
 
-    echo(f"{__banner__}", foreground="green", bold=True)
-    echo(f'[✓] With great power comes great responsibility', foreground="blue", bold=True)
+    echo(f"{__banner__}", fg="green", bold=True)
+    echo(f'[✓] With great power comes great responsibility', fg="blue", bold=True)
     self.tool_footer()
 
 
   @classmethod
   def install_tools(self):
-    echo(f'#############################################', foreground='black', background='cyan')
-    echo(f'//////////////SELECT YOUR TOOL///////////////', foreground='red', background='white') 
-    echo(f'#############################################', foreground='black', background='cyan')
+    echo(f'#############################################', fg='black', bg='cyan')
+    echo(f'//////////////SELECT YOUR TOOL///////////////', fg='red', bg='white') 
+    echo(f'#############################################', fg='black', bg='cyan')
 
   @classmethod
   def already_installed(self,name):
     self.tool_header()
-    echo(f"[ + ] Sorry, {name} is already installed!", foreground="cyan")
+    echo(f"[ + ] Sorry, {name} is already installed!", fg="cyan")
     self.tool_footer()
 
   @classmethod
@@ -159,40 +159,40 @@ class logo:
   @classmethod
   def not_installed(self,name):
     self.tool_header()
-    echo(f"[ x ] Sorry, {name} has not been installed!", foreground="vred") 
+    echo(f"[ x ] Sorry, {name} has not been installed!", fg="vred") 
     self.tool_footer()
 
   @classmethod
   def back(self):
-    echo(f'#############################################', foreground='vblack', background='vcyan')
-    echo(f'00) Go back', bold=True, foreground='vyellow') 
-    echo(f'#############################################', foreground='vblack', background='vcyan')
+    echo(f'#############################################', fg='vblack', bg='vcyan')
+    echo(f'00) Go back', bold=True, fg='vyellow') 
+    echo(f'#############################################', fg='vblack', bg='vcyan')
 
   @classmethod
   def updating(self):
-    echo(f'#############################################', foreground='vblack', background='vcyan')
-    echo(f'//////////////////UPDATING///////////////////', foreground='vred', background='vwhite') 
-    echo(f'#############################################', foreground='vblack', background='vcyan')
+    echo(f'#############################################', fg='vblack', bg='vcyan')
+    echo(f'//////////////////UPDATING///////////////////', fg='vred', bg='vwhite') 
+    echo(f'#############################################', fg='vblack', bg='vcyan')
 
   @classmethod
   def installing(self):
-    echo(f'#############################################', foreground='vblack', background='vcyan')
-    echo(f'/////////////////INSTALLING//////////////////', foreground='vred', background='vwhite') 
-    echo(f'#############################################', foreground='vblack', background='vcyan')
+    echo(f'#############################################', fg='vblack', bg='vcyan')
+    echo(f'/////////////////INSTALLING//////////////////', fg='vred', bg='vwhite') 
+    echo(f'#############################################', fg='vblack', bg='vcyan')
 
   @classmethod
   def menu(self,total):
     self.tool_header()
-    echo(f'[ 1 ] Show all tools', background='vyellow', foreground='vblack')
-    echo(f'[ 2 ] Show all categories', background='vblack', foreground='vyellow')
-    echo(f'[ 3 ] Update sashay', foreground='vblack', background='vyellow')
-    echo(f'[ 4 ] About us', foreground='vyellow', background='vblack')
-    echo(f'[ x ] Exit sashay', foreground='vblack', background='vyellow')
+    echo(f'[ 1 ] Show all tools', bg='vyellow', fg='vblack')
+    echo(f'[ 2 ] Show all categories', bg='vblack', fg='vyellow')
+    echo(f'[ 3 ] Update sashay', fg='vblack', bg='vyellow')
+    echo(f'[ 4 ] About us', fg='vyellow', bg='vblack')
+    echo(f'[ x ] Exit sashay', fg='vblack', bg='vyellow')
     self.tool_footer()
 
   @classmethod
   def exit(self):
     self.tool_header()
-    echo(f'Geez...where are you going so soon?', foreground='black', background='vred')
-    echo(f'Anyway, hope to see you back soon', background="vred") 
+    echo(f'Geez...where are you going so soon?', fg='black', bg='vred')
+    echo(f'Anyway, hope to see you back soon', bg="vred") 
     self.tool_footer()
